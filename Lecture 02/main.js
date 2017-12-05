583573a764785bdd664ec99955604d74c6025453
sunnixx <sumair.hamza@gmail.com>
2017-12-05 19:24:27 +0500

    lecture 02

---
+++ Lecture 02/main.js
@@ -0,0 +1,57 @@
+// // let greeting = "Hello World";
+
+// let arr = [1, 2, 3, 1.1, 1.2, 1.2, false, "Hello World"];
+
+// // arr[0] = function() {
+// //     console.log('Hello World from function');
+// // }
+
+// // arr[0]();
+
+// // console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// let bool = true;
+
+// if (bool === false) {
+//     console.log("Bool is false");
+// } else {
+//     console.log("Bool is true");
+// }
+
+//Take input in JS
+// let input = prompt('Enter input');
+// console.log(input);
+
+// let arr = [1,2,13,5,123,5,1,31,2,4,6];
+// let input = parseInt(prompt('Enter array number'));
+// let isFound = false;
+
+// for(let i = 0; i<arr.length; i++){
+//     if(input === arr[i]) {
+//         console.log("Index: " + i + " Value: " + arr[i]);
+//         isFound = true;
+//     }
+// }
+
+// if(isFound === false) {
+//     console.error("No value was found");
+// }
+
+let arr = [];
+let input = prompt("Enter Input");
+
+arr.push(input);
+
+console.log(arr);
+
+
+
+
+
+
+
+
